87fd517ec0419d29be1f50734019fe86de4cc376
skwangawur <rafiadw@gmail.com>
2023-03-24 15:33:24 +0700

    add planet test on server

---
+++ server/src/routes/planest/planets.test.js
@@ -0,0 +1,20 @@
+const app = require("../../app");
+const request = require("supertest");
+const { loadPlanetsData } = require("../../models/planets.model");
+const { mongoConnect, mongoDisConnect } = require("../../services/mongo");
+
+describe("test GET /v1/planets", () => {
+  beforeAll(async () => {
+    await mongoConnect();
+    await loadPlanetsData();
+  });
+
+  test("should get 8 habitable planets", async () => {
+    const response = await request(app)
+      .get("/v1/planets")
+      .expect("Content-Type", /json/)
+      .expect(200);
+
+    expect(response.body).toHaveLength(8);
+  });
+});
